a7f1e4c70b0f7f3a1aa6b5b9e3006c4312b9069f
JacoBSmitH101 <jacobsmith2005@hotmail.com>
2024-11-20 21:43:31 +0000

    Remove unused ad-bot PM2 configuration and placeholder test script

--- pm2test.js
+++
@@ -1,8 +0,0 @@
-//placeholder code that will loop and crash every 10 seconds
-setInterval(function () {
-    console.log("Crashing in 10 seconds");
-    setTimeout(function () {
-        console.log("Crashing now");
-        process.exit(1);
-    }, 10000);
-}, 10000);
