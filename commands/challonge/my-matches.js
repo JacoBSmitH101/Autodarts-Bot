8d2d39cd78422016892b0734f24a7378f3b8d4cd
JacoBSmitH101 <jacobsmith2005@hotmail.com>
2025-02-21 23:37:53 +0000

    Fix typo in matches embed title for user display

--- commands/challonge/my-matches.js
+++ commands/challonge/my-matches.js
@@ -103,7 +103,7 @@ module.exports = {
             const embed = new EmbedBuilder()
                 .setColor(0x3498db)
                 .setTitle(
-                    `Matches WOWz for ${interaction.user.username} | Division ${groupNumber}`
+                    `Matches WOWzers for ${interaction.user.username} | Division ${groupNumber}`
                 )
                 .setDescription(`League: **${tournamentName}**`)
                 .setTimestamp();
