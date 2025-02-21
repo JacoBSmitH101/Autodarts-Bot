2ba40db1c87835bc8d103e6f44d36978e1e1257f
JacoBSmitH101 <jacobsmith2005@hotmail.com>
2025-02-21 23:33:23 +0000

    test

--- commands/challonge/my-matches.js
+++ commands/challonge/my-matches.js
@@ -103,7 +103,7 @@ module.exports = {
             const embed = new EmbedBuilder()
                 .setColor(0x3498db)
                 .setTitle(
-                    `Matches WOW for ${interaction.user.username} | Division ${groupNumber}`
+                    `Matches WOWz for ${interaction.user.username} | Division ${groupNumber}`
                 )
                 .setDescription(`League: **${tournamentName}**`)
                 .setTimestamp();
