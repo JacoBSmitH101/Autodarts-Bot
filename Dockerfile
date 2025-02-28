46fe0104a96dbf719d3fea067ad3a92f3bdc7ef7
JacoBSmitH101 <jacobsmith2005@hotmail.com>
2025-02-28 23:30:45 +0000

    init

--- Dockerfile
+++ Dockerfile
@@ -10,10 +10,15 @@ COPY . .
 # Install dependencies
 RUN npm install
 RUN npm install -g nodemon
-
+RUN npm install pm2 -g
 # Set environment variable for mode
 ARG NODE_ENV
 ENV NODE_ENV $NODE_ENV
 
 # Run the bot based on the environment
-CMD ["sh", "-c", "if [ \"$NODE_ENV\" = \"development\" ]; then nodemon index.js; else node index.js; fi"]
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+CMD ["/entrypoint.sh"]
+
+
+
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Kubernetes uses DNS-based service discovery. This will check how many pods are running.
+ACTIVE_PODS=$(kubectl get pods -l app=discord-bot-dev --field-selector=status.phase=Running | grep -v NAME | wc -l)
+
+if [ "$ACTIVE_PODS" -lt "2" ]; then
+    echo "🚀 This pod is the active leader! Starting Discord bot..."
+    exec pm2-runtime start index.js  # Start the bot only on the leader pod
+else
+    echo "⏳ Standby mode: Another pod is already active. Waiting for failover..."
+    sleep infinity  # Keep the container alive without running the bot
+fi
