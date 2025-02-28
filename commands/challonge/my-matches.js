68096c0fb3afd9de18674f794893ee523f7f49af
JacoBSmitH101 <jacobsmith2005@hotmail.com>
2025-02-28 10:48:00 +0000

    Fix title in match embed to correct user message formatting

--- commands/challonge/my-matches.js
+++ commands/challonge/my-matches.js
@@ -104,7 +104,7 @@ module.exports = {
             const embed = new EmbedBuilder()
                 .setColor(0x3498db)
                 .setTitle(
-                    `Your matches (${interaction.user.username}) | Division ${groupNumber} ${process.env.HOSTNAME}`
+                    `Yourtest matches (${interaction.user.username}) | Division ${groupNumber} ${process.env.HOSTNAME}`
                 )
                 .setDescription(`League: **${tournamentName}**`)
                 .setTimestamp();
