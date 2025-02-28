641efbb84876f6492933858390f458d7aa4c9917
JacoBSmitH101 <jacobsmith2005@hotmail.com>
2025-02-28 19:30:55 +0000

    oops

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ ACTIVE_PODS=$(kubectl get pods -l app=discord-bot-dev --field-selector=status.ph
 
 if [ "$ACTIVE_PODS" -lt "2" ]; then
     echo "🚀 This pod is the active leader! Starting Discord bot..."
-    exec pm2 start index.js  # Start the bot only on the leader pod
+    exec pm2-runtime start index.js --no-daemon  # Start the bot only on the leader pod
 else
     echo "⏳ Standby mode: Another pod is already active. Waiting for failover..."
     sleep infinity  # Keep the container alive without running the bot
