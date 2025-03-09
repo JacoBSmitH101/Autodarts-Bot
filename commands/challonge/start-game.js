e8d4d5bf51a052f4ca51a5a9b0c8adb78fe85aa7
JacoBSmitH101 <jacobsmith2005@hotmail.com>
2025-03-09 12:52:41 +0000

    Add comment to clarify purpose of interaction defer in start-game command

--- commands/challonge/start-game.js
+++ commands/challonge/start-game.js
@@ -20,6 +20,7 @@ module.exports = {
                 .setDescription("Make lobby private")
                 .setRequired(false)
         ),
+    //show up please
     async execute(interaction) {
         await interaction.deferReply({ ephemeral: false });
 
